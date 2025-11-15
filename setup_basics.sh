24c57321423b6fad4933f2deeaa3f1bd95599a47
Zachuttak0 <150483761+Zachuttak0@users.noreply.github.com>
2025-11-15 16:58:36 -0500

    Update setup_basics.sh

--- setup_basics.sh
+++ setup_basics.sh
@@ -2,10 +2,47 @@
 sudo apt-get install openjdk-17-jdk
 mkdir serverfiles
 cd serverfiles
-sudo wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.4.0/forge-1.20.1-47.4.0-installer.jar
+sudo wget maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.4.0/forge-1.20.1-47.4.0-installer.jar
 sudo java -jar forge-1.20.1-47.4.0-installer.jar --installServer
 echo "\n-Xmx12G" >> user_jvm_args.txt
 echo "eula=true" > eula.txt
 cd ..
+sudo wget raw.githubusercontent.com/Zachuttak0/Scrpits-For-Ubuntu/main/start_server.sh
+sudo wget raw.githubusercontent.com/Zachuttak0/Scrpits-For-Ubuntu/main/startup_run.sh
+sudo wget raw.githubusercontent.com/Zachuttak0/Scrpits-For-Ubuntu/main/stop_server.sh
+sudo chmod +x startup_run.sh
+sudo chmod +x start_server.sh
+sudo chmod +x stop_server.sh
+sudo bash -c 'cat <<EOF > /etc/systemd/system/myscript.service
+[Unit]
+Description=My custom startup script
+After=network.target
+
+[Service]
+ExecStart=/home/mcserver/startup_run.sh
+User=root
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+sudo systemctl enable myscript.service
+
+( crontab -l 2>/dev/null | grep -F "0 12 * * * /home/mcserver/start_server.sh" ) >/dev/null
+if [ $? -ne 0 ]; then
+    ( crontab -l 2>/dev/null; echo "0 12 * * * /home/mcserver/start_server.sh" ) | crontab -
+    echo "Cron job installed."
+else
+    echo "Cron job already exists."
+fi
+
+( crontab -l 2>/dev/null | grep -F "0 0 * * * /home/mcserver/stop_server.sh" ) >/dev/null
+if [ $? -ne 0 ]; then
+    ( crontab -l 2>/dev/null; echo "0 0 * * * /home/mcserver/stop_server.sh" ) | crontab -
+    echo "Cron job installed."
+else
+    echo "Cron job already exists."
+fi
+
 sudo apt install screen
 sudo snap install ngrok
