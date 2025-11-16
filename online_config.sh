e15c469098b5c0a04ed52e40c47570858400c83b
Zachuttak0 <150483761+Zachuttak0@users.noreply.github.com>
2025-11-16 15:31:09 -0500

    fix

--- online_config.sh
+++ online_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo git clone https://github.com/Zachuttak0/The-Config --depth 1 --branch=master ./
+sudo git clone https://github.com/Zachuttak0/The-Config --depth 1 --branch=main ./
 sudo rm -rf .git/
 echo "Online Configs"
--- online_mods.sh
+++ online_mods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo git clone https://github.com/Zachuttak0/The-Mods --depth 1 --branch=master ./
+sudo git clone https://github.com/Zachuttak0/The-Mods --depth 1 --branch=main ./
 sudo rm -rf .git/
 echo "Online Thing for Mods"
