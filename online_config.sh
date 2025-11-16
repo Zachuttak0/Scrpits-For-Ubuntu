3a13649bcdd59393f6acba10df99133a25bb2f3d
Zachuttak0 <150483761+Zachuttak0@users.noreply.github.com>
2025-11-16 15:02:38 -0500

    add

--- online_config.sh
+++ online_config.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo git clone https://github.com/Zachuttak0/The-Config --depth 1 --branch=master ./
+sudo rm -rf .git/
 echo "Online Configs"
--- online_mods.sh
+++ online_mods.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo git clone https://github.com/Zachuttak0/The-Mods --depth 1 --branch=master ./
+sudo rm -rf .git/
 echo "Online Thing for Mods"
