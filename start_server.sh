eb4b1aaaa9e6e562e0a932b5f1a0e795c9181ddd
Zachuttak0 <150483761+Zachuttak0@users.noreply.github.com>
2025-11-15 22:32:35 -0500

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-screen -mdS MCRunner bash -c "cd /home/mcserver/serverfiles/ && ./run.sh"
+SCREEN=/usr/bin/screen
+BASH=/bin/bash
+SERVER_DIR=/home/mcserver/serverfiles
+SESSION_NAME=MCRunner
+
+export HOME=/home/mcserver
+export USER=mcserver
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+$SCREEN -mdS $SESSION_NAME
+$SCREEN -S $SESSION_NAME -p 0 -X stuff 'cd /home/mcserver/serverfiles && sudo ./run.sh^M'
