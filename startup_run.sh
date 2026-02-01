14a08a09ee401a3b1d9a3058db90a8a4d7166401
Zachuttak0 <150483761+Zachuttak0@users.noreply.github.com>
2026-02-01 16:37:59 -0500

    Update startup_run.sh

--- startup_run.sh
+++ startup_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export TERM=xterm
-screen -mdS NGRunner bash -c "tailscale up && tailscale serve https:443 / http://localhost:80 && tailscale funnel 443 on && tailscale serve status"
 screen -mdS WBRunner bash -c "cd /home/mcserver/WebServer && python app.py"
+screen -L -Logfile /home/mcserver/trader/logs.log -mdS BTRunner bash -c "cd /home/mcserver/trader && python3 bot.py"
